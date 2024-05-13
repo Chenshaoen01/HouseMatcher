5e516e75fade74bb5e6e0df0534a0fb440cdd87a
SeanChen <chenshaoen01@gmail.com>
2024-05-14 00:55:54 +0800

    house form validate

--- HouseMatcher/wwwroot/js/valiate.js
+++ HouseMatcher/wwwroot/js/valiate.js
@@ -1,8 +1,9 @@
 ﻿
-function validateBeforeSubmit(targetValidDataList) {
+function validateBeforeSubmit(targetValidDataList, submitValueData) {
     let hasInValidColumn = false
     targetValidDataList.forEach(validateData => {
-        const isTargetColumnValid = valiateTargetColumn(validateData)
+        const taregtDataValue = submitValueData !== undefined ? submitValueData[validateData.columnKey] : $(`#${validateData.columnId}`).val()
+        const isTargetColumnValid = valiateTargetColumn(validateData, taregtDataValue)
         hasInValidColumn = hasInValidColumn || !isTargetColumnValid
     })
 
@@ -10,18 +11,17 @@ function validateBeforeSubmit(targetValidDataList) {
 }
 
 // 檢驗特定欄位特定規則，回傳：是否符合規則
-function valiateTargetColumn(validateData) {
+function valiateTargetColumn(validateData, targetValue) {
     let isValid = true
     for (let index = 0; isValid && index < validateData.rule.length; index++) {
-        isValid = validateWithRule(validateData, index)
+        isValid = validateWithRule(targetValue, validateData, index)
     }
 
     return isValid
 }
 
 // 檢驗特定欄位特定規則，回傳：是否符合規則
-function validateWithRule(validateData, ruleIndex) {
-    const targetValue = $(`#${validateData.columnId}`).val()
+function validateWithRule(targetValue, validateData, ruleIndex) {
     const ruleName = validateData.rule[ruleIndex]
 
     let isValidValue = true
@@ -30,6 +30,8 @@ function validateWithRule(validateData, ruleIndex) {
     } else if (ruleName === 'email') {
         var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
         isValidValue = emailRegex.test(targetValue)
+    } else if (ruleName === 'positive-number') {
+        isValidValue = targetValue > 0
     }
 
     let inValidHintText = ""
@@ -38,6 +40,8 @@ function validateWithRule(validateData, ruleIndex) {
             inValidHintText = `${validateData.columnName}為必填項目`
         } else if (ruleName === 'email') {
             inValidHintText = `${validateData.columnName}格式不符`
+        } else if (ruleName === 'positive-number') {
+            inValidHintText = `${validateData.columnName}須為正數`
         }
     }
     $(`#${validateData.validateTextId}`).text(inValidHintText)
